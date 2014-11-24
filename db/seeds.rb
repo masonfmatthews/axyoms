fd83822d577c141c2f087c06737b94881ca9c847
Mason F. Matthews <mason.f.matthews@gmail.com>
2014-11-24 09:18:58 -0500

    Clean up random seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ User.create!(name: "Mason",
              password: "password",
              password_confirmation: "password")
 
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@example.com"
-  Student.create!(name:  name, email: email)
-end
+# 99.times do |n|
+#   name = Faker::Name.name
+#   email = "example-#{n+1}@example.com"
+#   Student.create!(name:  name, email: email)
+# end
