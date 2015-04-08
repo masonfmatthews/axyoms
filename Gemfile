ac504162ac9bf3d9665a7728d92467270826f7fe
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-04-07 21:15:41 -0400

    Modify concepts based on first round of experience.

--- Gemfile
+++ Gemfile
@@ -41,6 +41,3 @@ end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
-# Use unicorn as the app server
-# gem 'unicorn'
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,3 @@ User.create!(name: "Mason",
              email: "mason@example.com",
              password: "password",
              password_confirmation: "password")
-
-# 99.times do |n|
-#   name = Faker::Name.name
-#   email = "example-#{n+1}@example.com"
-#   Student.create!(name:  name, email: email)
-# end
