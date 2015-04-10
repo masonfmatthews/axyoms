9b2bd53f31cb785273402f2c79e2634bb11e3d5f
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-04-10 12:25:22 -0400

    Add Students

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ User.create!(name: "Mason",
              password: "password",
              password_confirmation: "password")
 
-unit_hash = {"Programming Languages" => ["Control Flow", "Variables"]}
+unit_hash = {"Wk1: Mon" => ["Control Flow", "Variables"],
+  "Wk2: Tue" => ["Methods", "Arrays"]}
 
 first_day = "2015-04-04".to_date
 first_week = (0..3).to_a.map {|i| first_day + i.days}
@@ -17,4 +18,11 @@ unit_hash.each do |k, v|
     u.unit_coverages << UnitCoverage.new(concept_uuid: Concept.where(name: concept_name).first.uuid)
   end
   u.save!
+end
+
+student_names = ["John", "Peter", "Scott", "Anna", "Danai", "Daisy", "Zack",
+    "Aaron", "JohnB", "Cruz", "Turner", "Tamika", "Nathaniel"]
+
+student_names.each do |n|
+  Student.create!(name: n, email: "#{n}@#{n}.com")
 end
