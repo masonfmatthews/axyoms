4a8482907109bd535674e89323e0ff861e37495e
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-04-10 11:18:44 -0400

    Restrict access to concept and unit controls

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -1,4 +1,6 @@
 class ConceptsController < ApplicationController
+  before_action :logged_in_user, except: [:summary]
+
   def summary
     @concept = Concept.find(params[:uuid])
     render partial: "summary"
