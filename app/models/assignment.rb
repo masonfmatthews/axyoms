7d83626d76dd543fa9c267227e000d5d6471c842
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-04-13 13:21:13 -0400

    Add average scores to concept summary box

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -6,4 +6,13 @@ class Assignment < ActiveRecord::Base
 
   validates :name, presence: true
   validates :uri, presence: true, format: {with: /\Ahttps?:\/\//, message: "must start with http:// or https://"}
+
+  def average_score
+    (scores.reduce(0.0) {|sum, s| sum + s.score})/(scores.count)
+  end
+
+  def average_score_for_concept_uuid(uuid)
+    concept_scores = scores.where(concept_uuid: uuid)
+    (concept_scores.reduce(0.0) {|sum, s| sum + s.score})/(concept_scores.count)
+  end
 end
