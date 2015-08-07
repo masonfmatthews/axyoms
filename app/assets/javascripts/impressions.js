809afc0fe9eef41471a31df68ef8060a8b602c55
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-08-07 10:31:26 -0400

    Complete JavaScript for impression scoring.

--- app/assets/javascripts/impressions.js
+++ app/assets/javascripts/impressions.js
@@ -1,9 +1,38 @@
-function bindImpressions() {
+function hideLastImpressions() {
+  var groups = $(".impression-group");
+  for(i=0; i < groups.length; i++) {
+    $(groups[i]).find(".impression-wrapper").last().hide();
+  };
+}
+
+function removeImpression() {
+  var link = $(event.target);
+  link.closest(".impression-wrapper").remove();
+}
+
+function bindRemoveImpression() {
+  $(".delete-impression").on("click", removeImpression);
+}
+
+function duplicateImpression() {
+  var link = $(event.target);
+  var impressionGroup = link.closest("td").find(".impression-group");
+  var hiddenWrapper = impressionGroup.find(".impression-wrapper").last();
+  impressionGroup.append(hiddenWrapper.clone());
+  hiddenWrapper.show();
+  bindRemoveImpression
+}
+
+function bindDuplicateImpression() {
+  $(".add-impression").on("click", duplicateImpression);
+}
+
+function setUpImpressions() {
   if($(".edit-scores").length > 0) {
-    $(".add-impression").on("click", function(){
-      alert("Yo");
-    });
+    hideLastImpressions();
+    bindRemoveImpression();
+    bindDuplicateImpression();
   }
 }
 
-$(bindImpressions)
+$(setUpImpressions)
